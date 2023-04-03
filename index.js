f3577be0152e4a9b1a96cbb46c15e3329d805f34
unknown <tombirbeck47@gmail.com>
2023-04-03 15:52:36 +0100

    added test function to check connection and return db list

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import { MongoClient } from 'mongodb';
-const client = new MongoClient(process.env.CONNECTION_URI)
+import 'dotenv/config';
+
+async function listDatabases(client) {
+    const databasesList = await client.db().admin().listDatabases();
+    console.log("Databases: ");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
 
 const main = async () => {
 
+    const client = new MongoClient(process.env.CONNECTION_URI);
+    
+    try {
+        await client.connect();
+        await listDatabases(client);
+    
+    } catch(e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+    
+
 }
+
+main().catch(console.error);
