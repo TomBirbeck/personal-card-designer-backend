24f72a7b3f37c9e75ca09f08f893c64f9c26ba41
unknown <tombirbeck47@gmail.com>
2023-04-05 16:00:41 +0100

    fixed incorrect client in app

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes.js'
 import 'dotenv/config';
+import { MongoClient } from 'mongodb';
+const client = new MongoClient(process.env.CONNECTION_URI)
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,10 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/', router);
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology: true })
+client.connect(process.env.CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, ()=>console.log(`Server Running on Port ${PORT}`)))
     .catch((error) => console.log(error.message));
-
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
 
 export default app
