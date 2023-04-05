ec244194101d174fd791ab49bee5cf77a164dd53
unknown <tombirbeck47@gmail.com>
2023-04-05 16:40:06 +0100

    removed connection in app js as it caused deployment to fail

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes.js'
 import 'dotenv/config';
-import { MongoClient } from 'mongodb';
-const client = new MongoClient(process.env.CONNECTION_URI)
+// import { MongoClient } from 'mongodb';
+// const client = new MongoClient(process.env.CONNECTION_URI)
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,8 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/', router);
 
-client.connect(process.env.CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, ()=>console.log(`Server Running on Port ${PORT}`)))
-    .catch((error) => console.log(error.message));
+// client.connect(process.env.CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology: true })
+//     .then(() => app.listen(PORT, ()=>console.log(`Server Running on Port ${PORT}`)))
+//     .catch((error) => console.log(error.message));
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
 
 export default app
