2f2c5343ac9cdc853c6ef18a6f7489e9cfc77f06
unknown <tombirbeck47@gmail.com>
2023-04-05 15:48:48 +0100

    merging route + model logic for getting designs to see if issue is where it's connecting

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/', router);
 
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, ()=>console.log(`Server Running on Port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
 
 export default app
--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-// import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import 'dotenv/config';
 import { addNewDesign, findDesigns } from "./models.js";
 
+const client = new MongoClient(process.env.CONNECTION_URI);
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -16,13 +17,24 @@ router.get('/', (req, res, next) => {
 
 router.get('/designs', async (req, res, next) => {
     try {
-        const data = await findDesigns("u_card", "designs");
+        await client.connect()
+        const data = await client.db("u_card").collection("designs").find().limit(20).toArray();
         res.json({success: true, data: data})
     } catch(error) {
         res.json({success: false, data: error})
     }
     next()
 })
+
+// router.get('/designs', async (req, res, next) => {
+//     try {
+//         const data = await findDesigns("u_card", "designs");
+//         res.json({success: true, data: data})
+//     } catch(error) {
+//         res.json({success: false, data: error})
+//     }
+//     next()
+// })
 
 router.post('/designs', async (req, res, next) => {
     try {
